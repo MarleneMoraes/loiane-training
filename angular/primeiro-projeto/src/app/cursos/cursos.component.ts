629feba19ee8fdf57770e8e6f0ada41d4518d3b4
Marlene Moraes <marlenevmoraes@outlook.com>
2023-04-23 09:55:17 -0300

    class 006 - templates

--- angular/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ angular/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'meu-primeiro-component',
+  // template literal: utilizado apenas se o codigo HTML tiver ate 3 linhas
   template: `
     <p>Meu primeiro component com Angular 2!</p>
   `
