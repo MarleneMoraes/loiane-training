3124eb13d5b7502d545975699f71c47665dd1639
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-01 21:22:52 +0000

    class 56 - abstract methods w/ enums

---
+++ java-intermediario/src/com/loiane/cursojava/aula56/TesteDocumento.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula56;
+
+public class TesteDocumento {
+    public static void main(String[] args) {
+
+        Pessoa pessoaFisica = new Pessoa();
+        pessoaFisica.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CPF"));
+        pessoaFisica.setNumeroDocumento(pessoaFisica.getTipoDocumento().geraNumeroTeste());
+        System.out.println(pessoaFisica);
+
+        Pessoa pessoaJuridica = new Pessoa();
+        pessoaJuridica.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CNPJ"));
+        pessoaJuridica.setNumeroDocumento(pessoaJuridica.getTipoDocumento().geraNumeroTeste());
+        System.out.println(pessoaJuridica);
+    }
+}
