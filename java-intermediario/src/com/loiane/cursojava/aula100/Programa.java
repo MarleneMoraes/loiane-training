0c0bf5bf533e4dda2d232025b06d35405a143137
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-10 19:17:36 -0300

    class 100 - import jar files

---
+++ java-intermediario/src/com/loiane/cursojava/aula100/Programa.java
@@ -0,0 +1,11 @@
+package com.loiane.cursojava.aula100;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Programa {
+	public static void main(String[] args) {
+
+		System.out.println(StringUtils.capitalize("loiane"));
+
+	}
+}
