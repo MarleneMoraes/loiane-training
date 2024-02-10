08a8095cf7f1390e163ad32c335af898b3195d3e
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-10 14:11:01 -0300

    class 73 - threads: resume, suspend, stop

---
+++ java-intermediario/src/com/loiane/cursojava/aula73/Teste.java
@@ -0,0 +1,41 @@
+package com.loiane.cursojava.aula73;
+
+public class Teste {
+	public static void main(String[] args) {
+
+		MinhaThread thread1 = new MinhaThread("#1");
+		MinhaThread t2 = new MinhaThread("#2");
+
+		System.out.println("Pausando a Thread #1");
+
+		thread1.suspend();
+
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Pausando a Thread #2");
+
+		t2.suspend();
+
+		System.out.println("Resumindo a Thread #1");
+
+		thread1.resume();
+
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Resumindo a Thread #2");
+
+		t2.resume();
+
+		System.out.println("Terminando a Thread #2");
+
+		t2.stop();
+	}
+}
