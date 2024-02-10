f1cad40110786ed05a2c62f6a8357893d95aaf22
Marlene Moraes <marlenevmoraes@outlook.com>
2024-02-10 15:30:52 -0300

    class 81 - string: toLowerCase and toUpperCase methods

---
+++ java-intermediario/src/com/loiane/cursojava/aula81/Programa.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula81;
+
+public class Programa {
+	public static void main(String[] args) {
+
+		String teste = "Teste";
+
+		// Transformacao para minusculos (lower) ou maiusculos (upper)
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);
+	}
+}
